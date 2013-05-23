61edaedc9ef70ba6ca4bef7c5075e54ed65f9e10
Mario Ibarra <mariolp13@gmail.com>
2013-05-23 14:14:49 -0400

    ws integrado

--- proyecto.php
+++ proyecto.php
@@ -1,14 +1,38 @@
 <?php
 
+include('lib/nusoap.php');
+
 $u= $_POST['user'];
 $p= $_POST['pass'];
 
-//Guarda en $srt el valor de la contraseña en mayuscula y codificada en SHA256 que es lo que soporta dirdoc, lista para comprobar en el WS
+//Guarda en $srt el valor de la contraseña en mayuscula y codificada en SHA256
  $str = hash("SHA256", $p);
-      
-echo "Valor en SHA256: ".$str;
+ 
+//Creacion de un arreglo       
+$parametros=array();
+$parametros['rut']= $u;
+$parametros['password']= $str;
+
+$objClienteSOAP = new soapclient("http://informatica.utem.cl:8011/dirdoc-auth/ws/auth?wsdl");
+$objRespuesta = $objClienteSOAP->autenticar($parametros);
+
+if ($objRespuesta == true)
+
+{
+	echo "Ingreso exitoso";
+}
+
+else
+{
+	echo "Rechazado";
+}
+
+
+
+                               
 
 ?>
+
 
 
 
