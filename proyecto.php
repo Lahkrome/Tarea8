0517e18419407b9387ad8a95a8303754480fd256
lain21 <batusay-16@hotmail.com>
2013-05-23 19:33:37 -0400

    Update proyecto.php
    quitar puntos

--- proyecto.php
+++ proyecto.php
@@ -8,9 +8,12 @@ $p= $_POST['pass'];
 //Guarda en $srt el valor de la contraseña en mayuscula y codificada en SHA256
  $str = hash("SHA256", $p);
  
+ $sp = str_replace('.', '-', $u); 
+ 
+ 
 //Creacion de un arreglo       
 $parametros=array();
-$parametros['rut']= $u;
+$parametros['rut']= $sp;
 $parametros['password']= $str;
 
 $objClienteSOAP = new soapclient("http://informatica.utem.cl:8011/dirdoc-auth/ws/auth?wsdl");
